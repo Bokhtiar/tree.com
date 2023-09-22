e5e5a74f62e260bfa01c13afdb23ff5dfd25e8d6
bokhtiar <bokhtiar@gmail.com>
2023-09-22 19:50:09 +0600

    navbar && footer components create

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -1,13 +1,15 @@
+import { Footer } from '../components/footer';
 import { Outlet } from "react-router-dom";
+import { Navbar } from '../components/navbar';
 
 export const MainLayout = () => {
     return (
         <>
-            <>navbar</>
+            <Navbar />
             <div className="">
                 <Outlet />
             </div>
-            <>footer</>
+            <Footer />
         </>
     );
 };
