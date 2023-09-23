cbe04fc3e911484692039713995c6885bdf4bdf8
bokhtiar <bokhtiar@gmail.com>
2023-09-23 20:23:38 +0600

    Dashboard home page desing

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { Cart } from "../components/cart"
 import { Navigate } from "react-router-dom"
 import { Wishlish } from "../components/wishlist"
 import { DashboardLayout } from "../layouts/dashboard.layout"
+import { Dashboard } from '../components/dashboard'
 
 const appRoutes = [
     {
@@ -10,7 +11,7 @@ const appRoutes = [
         element: <DashboardLayout />,
         children: [
             { path: "*", element: <Navigate to="/404" /> },
-            { path: "", element: <>dashboard</> },
+            { path: "", element: <Dashboard /> },
             { path: "cart", element: <Cart /> },
             { path: "wishlist", element: <Wishlish /> },
             { path: "order", element: <Order /> },
